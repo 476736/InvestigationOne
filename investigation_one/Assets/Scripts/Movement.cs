9fe9569f3ffc9c17e333b2bf7c73dd8917a61d4f
Sachta291 <476736@chrome.escg.ac.uk>
2019-10-04 10:08:26 +0100

    help

---
+++ investigation_one/Assets/Scripts/Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    //Basic movement controls
+    public int jumpConst = 0;
+
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+
+    public bool isGrounded;
+    void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        MovementFunc();
+        Jumping();
+
+    }
+
+    private void Jumping()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && isGrounded)
+        {
+            selfRigidbody.AddForce(0,jumpConst, 0, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void MovementFunc()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.back * 3);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.forward * 3);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce(Vector3.left * 3);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce(Vector3.right * 3);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
---
+++ investigation_one/Assets/Scripts/Respawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject spawnPoint;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Enter");
+        if (col.transform.tag == "Enemy")
+        {
+            Debug.Log("colided with enemy");
+            this.transform.position = spawnPoint.transform.position;
+        }
+    }
+}
+
+
+
